18918486b15ac0a7c8c7c320a2bf2c090aaa4897
Mira Vogt <miravogt@gmail.com>
2020-11-11 15:57:43 +0100

    maps project done

---
+++ src/Company.ts
@@ -0,0 +1,30 @@
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+    name: string;
+    catchPhrase: string;
+    location: {
+        lat: number;
+        lng: number
+    }
+
+    constructor() {
+        this.name = faker.company.companyName();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+
+    }
+
+    markerContent(): string {
+        return `
+        <div>
+        <h3>Company Name: ${this.name}<h3>
+        <h5>Catchphrase: ${this.catchPhrase}<h5>
+        </div>
+        `;
+    }
+}
